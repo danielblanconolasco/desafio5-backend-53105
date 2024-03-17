3102339d31f456f4a9231ac177ec534c3872d6c8
Daniel Blanco <danielblanconolasco@gmail.com>
2024-03-17 04:53:08 +0200

    limpieza

--- src/app.js
+++ src/app.js
@@ -92,7 +92,7 @@ io.on("connection", async (socket) => {
 
 
         //Obtengo los mensajes de MongoDB y se los paso al cliente:
-        const messages = await ChatModel.find();
+        const messages = await ChatModel.find()
         io.sockets.emit("message", messages)
         console.log(messages)
     })
